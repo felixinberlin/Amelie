242eef993ce75e3a8cb7af22e581d658560e8ed8
felix <felix@tortilladepatas.de>
2026-09-22 13:23:08 +0200

    tsts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: './',
+   base: '/Amelie/',
   plugins: [react(), tailwindcss()],
   resolve: {
     dedupe: ['react', 'react-dom'],
